780160a4fc1ff4ff6ce4a5c80d41629b774f258b
Jack Patterson <jtyler03@optonline.net>
2020-03-03 17:16:16 -0800

    sound

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,6 +7,13 @@
 
 package frc.robot.subsystems;
 
+import java.io.ByteArrayOutputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -21,6 +28,15 @@ public class Shooter extends SubsystemBase {
   public CANSparkMax shooter = new CANSparkMax(Constants.SHOOT_MOTOR, MotorType.kBrushless);
   public DoubleSolenoid shooterSOL = new DoubleSolenoid(Constants.SHOOTER_FORWARD, Constants.SHOOTER_REVERSE);
 
+  ByteArrayOutputStream byteArrayOutputStream;
+  TargetDataLine targetDataLine;
+  int cnt;
+  boolean stopCapture = false;
+  byte tempBuffer[] = new byte[8000];
+  int countzero, countdownTimer;    
+  short convert[] = new short[tempBuffer.length];
+ 
+
   public Shooter() {
 
   }
@@ -30,6 +46,45 @@ public class Shooter extends SubsystemBase {
     SmartDashboard.putBoolean("Valid", true);
 
   }
+
+  public double getSound(){
+    try {
+      byteArrayOutputStream = new ByteArrayOutputStream();
+      stopCapture = false;
+      countdownTimer = 0;
+      while (!stopCapture) {
+          AudioFormat audioFormat = new AudioFormat(8000.0F, 16, 1, true, false);
+          DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, audioFormat);
+          targetDataLine = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
+          targetDataLine.open(audioFormat);
+          targetDataLine.start();
+          cnt = targetDataLine.read(tempBuffer, 0, tempBuffer.length);
+          byteArrayOutputStream.write(tempBuffer, 0, cnt); 
+          try {
+              countzero = 0;
+              for (int i = 0; i < tempBuffer.length; i++) {                                     
+                  convert[i] = tempBuffer[i];
+                  if (convert[i] == 0) {
+                      return countzero++;
+                  }
+              }
+               
+              countdownTimer++;
+              System.out.println(countzero);
+
+          } catch (StringIndexOutOfBoundsException e) {
+              System.out.println(e.getMessage());
+          }
+          Thread.sleep(0);
+          targetDataLine.close();
+      }
+  } catch (Exception e) {
+      System.out.println(e);
+  }
+
+  return countzero;
+
+}
 
   public double getVelocity(){
     return shooter.getEncoder().getVelocity();
